d93c0514526dac72a8fff814a8bbacf0bb7d7615
Asirpithanand <99887217+Asirpithanand@users.noreply.github.com>
2022-06-08 11:44:44 +0530

    Update notify_app.py
    new update fixed and added again

--- notify_app.py
+++ notify_app.py
@@ -20,8 +20,16 @@ while True:
                     )
                 time.sleep(900) #time has been set so that every 2mins (120sec) the programme will execute, while the contiton is true
 
+                
         elif info2 == False: # this condition is to make sure the programme has been ended 
             print(batteryinfo)
             print('Programme finished')
+            notification.notify(
+                        title='NOTIFY',
+                        message= f"Battery AT | {info1}% ",
+                        app_name='NOTIFY',
+                        timeout=10, # the notification will stay on screen for 10 sec
+                        app_icon = " " #copy image path and paste it here
+                    )
             break # because this elif condition  is inside a while loop it is important to break the loop after the programme is executed 
-        
+            
