159e1d355e4866be1541d30789cdce2375940224
Asirpithanand <99887217+Asirpithanand@users.noreply.github.com>
2022-06-09 22:30:10 +0530

    Update notify_app.py
    new code updated along with WhatsApp notification

--- notify_app.py
+++ notify_app.py
@@ -1,14 +1,23 @@
+# importing requried modules
+
 from plyer import battery, notification
 import time
+from datetime import datetime
+import pywhatkit as msg
+
+
+batteryinfo = battery.status   #this will show the current battery percent  and  whether battery is charging or not
+# stored the information in separate variable
+info1 = batteryinfo.get("percentage") 
+info2 = batteryinfo.get("isCharging") 
+
+now = datetime.now()
+hour = int(now.strftime("%H"))
+minute = int(now.strftime("%M")) + 1 
+
 
 
 while True:
-# def batteryHealth():
-        batteryinfo = battery.status   #this will show the current battery percent  and  whether battery is charging or not
-        # stored the information in separate variable
-        info1 = batteryinfo.get("percentage") 
-        info2 = batteryinfo.get("isCharging") 
-
         #checking the condition of battery percent and charging status
         if info1 >= 90 and info2 == True:
                 notification.notify(
@@ -16,20 +25,11 @@ while True:
                         message= f"Battery At {info1}% | Disconnect Charger",
                         app_name='NOTIFY',
                         timeout=10, # the notification will stay on screen for 10 sec
-                        app_icon = " " #copy image path and paste it here
+                        app_icon = " " #  download & past the path of the imgae which is in the repo
                     )
-                time.sleep(900) #time has been set so that every 2mins (120sec) the programme will execute, while the contiton is true
+                msg.sendwhatmsg('enter your phone number here along with country code', f"Battery At {info1}% | Disconnect Charger", hour, minute)
+                print('program finished')
+                break
+        time.sleep(120) #time has been set so that every 2mins (120sec) the programme will execute, while the contiton is true
 
-                
-        elif info2 == False: # this condition is to make sure the programme has been ended 
-            print(batteryinfo)
-            print('Programme finished')
-            notification.notify(
-                        title='NOTIFY',
-                        message= f"Battery AT | {info1}% ",
-                        app_name='NOTIFY',
-                        timeout=10, # the notification will stay on screen for 10 sec
-                        app_icon = " " #copy image path and paste it here
-                    )
-            break # because this elif condition  is inside a while loop it is important to break the loop after the programme is executed 
-            
+
