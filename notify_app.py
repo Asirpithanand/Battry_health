f478d5aeb691a2db47683539cd1a65662fe0689d
Asirpithanand <99887217+Asirpithanand@users.noreply.github.com>
2022-06-10 09:18:12 +0530

    Update notify_app.py
    new updated fixed

--- notify_app.py
+++ notify_app.py
@@ -1,9 +1,21 @@
 # importing requried modules
 
+import urllib.request
 from plyer import battery, notification
 import time
 from datetime import datetime
-import pywhatkit as msg
+
+def wifi(host='http://google.com'):
+    """chekcing the connection of internet via google search as google is reliable"""
+    try:
+        urllib.request.urlopen(host)  
+        return True
+    except:
+        return False
+connected = wifi()
+
+if connected == True:
+    import pywhatkit as msg
 
 
 batteryinfo = battery.status   #this will show the current battery percent  and  whether battery is charging or not
@@ -16,20 +28,32 @@ hour = int(now.strftime("%H"))
 minute = int(now.strftime("%M")) + 1 
 
 
-
 while True:
+        if info2 == False:
+            notification.notify(
+                        title='NOTIFY',
+                        message= f"Battery At | {info1}%",
+                        app_name='NOTIFY',
+                        timeout=10, # the notification will stay on screen for 10 sec
+                        app_icon = " "  # copy and paste the path of the image attached with this repo
+                    )
+            break    
+
         #checking the condition of battery percent and charging status
-        if info1 >= 90 and info2 == True:
-                notification.notify(
+        elif info1 >= 90 and info2 == True:
+                if connected == True:
+                    msg.sendwhatmsg('enter your whatsapp number here along with country code', f"Battery At {info1}% | Disconnect Charger", hour, minute)
+                elif connected == False:
+                    notification.notify(
                         title='NOTIFY',
                         message= f"Battery At {info1}% | Disconnect Charger",
                         app_name='NOTIFY',
                         timeout=10, # the notification will stay on screen for 10 sec
-                        app_icon = " " #  download & past the path of the imgae which is in the repo
+                        app_icon = " " # copy and paste the path of the image attached with this repo
                     )
-                msg.sendwhatmsg('enter your phone number here along with country code', f"Battery At {info1}% | Disconnect Charger", hour, minute)
+                    break
                 print('program finished')
                 break
-        time.sleep(120) #time has been set so that every 2mins (120sec) the programme will execute, while the contiton is true
+        time.sleep(120) #timer has been set so that every 2mins (120sec) the program will execute, while the contiton is true
 
 
